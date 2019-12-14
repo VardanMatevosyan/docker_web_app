ff16217ea59d67f9f9587eb6c159ef2b538f86b1
VardanMatevosyan <vardanmt@gmail.com>
2019-12-14 21:19:50 +0200

    add hello controller

---
+++ src/main/java/com/hello/demo/DemocController.java
@@ -0,0 +1,13 @@
+package com.hello.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemocController {
+    @GetMapping
+    public String getHello() {
+        return "Hello";
+    }
+}
